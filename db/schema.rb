1935f1c700ac62963919b21ceebf333174b3c682
Gingertonic <bethmschofield@gmail.com>
2019-12-04 11:55:12 +0000

    add favourite to song

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_104238) do
+ActiveRecord::Schema.define(version: 2019_12_04_115019) do
 
   create_table "songs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_12_04_104238) do
     t.string "fullText"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "favourite"
   end
 
 end
